45522dbd4603574e4f2ce0220daf9e110d94190d
Matias <matiasmoron008@gamil.com>
2020-03-11 15:50:09 -0300

    Updates

---
+++ precache-manifest.401d3a869af33e7c042217475bb2a5f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a64967b9d9d67eb3438eccd948e1e5f",
+    "url": "/matiasmoron/matiasmoron-cv/index.html"
+  },
+  {
+    "revision": "672ea96405c61750f07b",
+    "url": "/matiasmoron/matiasmoron-cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b28968f9ea1fae9cabc",
+    "url": "/matiasmoron/matiasmoron-cv/static/js/2.107e50a9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/matiasmoron/matiasmoron-cv/static/js/2.107e50a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "672ea96405c61750f07b",
+    "url": "/matiasmoron/matiasmoron-cv/static/js/main.3d29d264.chunk.js"
+  },
+  {
+    "revision": "38d475af8689edc9748a",
+    "url": "/matiasmoron/matiasmoron-cv/static/js/runtime-main.282cd464.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/matiasmoron/matiasmoron-cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
