3a11c4de7721c8ab908573fdc6d392ffae919060
Matias <matiasmoron008@gamil.com>
2020-03-11 15:59:00 -0300

    Updates

---
+++ precache-manifest.073e13cc011c5a3b5771c024f2979468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bda056f56cd6bd00ed299a87682b891",
+    "url": "/matiasmoron-cv/index.html"
+  },
+  {
+    "revision": "719e9321183a84ecdcc3",
+    "url": "/matiasmoron-cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b28968f9ea1fae9cabc",
+    "url": "/matiasmoron-cv/static/js/2.107e50a9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/matiasmoron-cv/static/js/2.107e50a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719e9321183a84ecdcc3",
+    "url": "/matiasmoron-cv/static/js/main.9cadfba0.chunk.js"
+  },
+  {
+    "revision": "2903ffff7265440bb23d",
+    "url": "/matiasmoron-cv/static/js/runtime-main.2ec8256b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/matiasmoron-cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/matiasmoron/matiasmoron-cv/precache-manifest.401d3a869af33e7c042217475bb2a5f0.js"
+  "/matiasmoron-cv/precache-manifest.073e13cc011c5a3b5771c024f2979468.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/matiasmoron/matiasmoron-cv/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/matiasmoron-cv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
